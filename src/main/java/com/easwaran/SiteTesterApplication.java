ac8b8ce3ee3b81900728bb6899a82ebb670230f6
Easwaran <rajkumar26041997@gmail.com>
2020-12-28 00:07:17 +0530

    Initial Commit

---
+++ src/main/java/com/easwaran/SiteTesterApplication.java
@@ -0,0 +1,13 @@
+package com.easwaran;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiteTesterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SiteTesterApplication.class, args);
+	}
+
+}
